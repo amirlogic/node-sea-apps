b398f4b3d006269c36d8e085a73e54853ea17059
Amir <amir.hac@outlook.com>
2025-02-11 22:43:48 +0100

    bgcolor

--- http.js
+++ http.js
@@ -34,6 +34,9 @@ function webpage(title='untitled',xhead='',body=''){
                                 <title>${title}</title>
                                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
                                 <meta charset="UTF-8">
+                                <style>
+                                    body{ background-color:#f2f2f2; }
+                                </style>
                                 ${xhead}
                             </head>
                             <body>
